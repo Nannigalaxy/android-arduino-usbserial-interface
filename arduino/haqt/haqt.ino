ff015077acd680868f132f7a6bd95a3bf961463f
Nandan Manjunatha <Nannigalaxy@gmail.com>
2021-04-24 12:37:47 +0530

    Add arduino code and library

---
+++ arduino/haqt/haqt.ino
@@ -0,0 +1,26 @@
+#include <dht11.h>
+
+#define dht_pin A1
+#define mq_pin A0
+
+dht11 DHT11;
+int mq_val;
+
+void setup(){  
+  
+Serial.begin(9600); 
+ }
+ 
+ 
+void loop(){
+//  if(Serial.available()){
+int chk = DHT11.read(dht_pin);
+mq_val = analogRead(0);
+uint8_t b = mq_val;
+
+Serial.write(DHT11.humidity);
+Serial.write(DHT11.temperature);
+Serial.write(b);       
+//}
+delay(1000);                                  
+}
